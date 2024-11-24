88335789e6ab27b0264b017d374a8e66cb0a384d
Aleksandr Mertsalov <skrrydg@ya.ru>
2024-11-24 13:40:19 +0400

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name='skrrydg-ab-ml-challenge',
+  name='skrrydg_ab_ml_challenge',
   version='0.0.1',
   packages=find_packages(where="src"),
   package_dir={"": "src"},
