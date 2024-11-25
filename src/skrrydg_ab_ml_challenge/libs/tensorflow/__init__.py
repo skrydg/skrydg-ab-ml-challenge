e1da773c744ac6e4d33813195bfadf6b8838e0ab
Aleksandr Mertsalov <skrrydg@ya.ru>
2024-11-25 23:59:09 +0400

    fix

--- src/skrrydg_ab_ml_challenge/libs/tensorflow/__init__.py
+++ src/skrrydg_ab_ml_challenge/libs/tensorflow/__init__.py
@@ -1,4 +1,5 @@
 from .dataset import *
 # from .kfold_model import *
 from .pretrained_model import *
-# from .time_series_kfold import *
+from .backtest_loss import *
+from .backtest_metric import *
