2642785015c35a18c7e19d7169254650c7f11d47
Aleksandr Mertsalov <skrrydg@ya.ru>
2024-11-26 00:02:54 +0400

    fix

--- src/skrrydg_ab_ml_challenge/libs/kfold.py
+++ src/skrrydg_ab_ml_challenge/libs/kfold.py
@@ -5,7 +5,7 @@ import sklearn
 class KFold:
     def __init__(self, n_splits):
         self.n_splits = n_splits
-        self.splitter = sklearn.model_selection.KFold(n_splits, random_state=42)
+        self.splitter = sklearn.model_selection.KFold(n_splits)
         
     def split(self, df):
        yield from self.splitter.split(df, None)
