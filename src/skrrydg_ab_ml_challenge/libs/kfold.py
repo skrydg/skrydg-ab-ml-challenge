fdbee925b3da5412ded08760f2100769ab9aaa4c
Aleksandr Mertsalov <skrrydg@ya.ru>
2024-12-01 10:02:50 +0400

    fix

--- src/skrrydg_ab_ml_challenge/libs/kfold.py
+++ src/skrrydg_ab_ml_challenge/libs/kfold.py
@@ -8,4 +8,7 @@ class KFold:
         self.splitter = ms.KFold(n_splits)
         
     def split(self, df):
-       yield from self.splitter.split(df, None)
+        for i in range(5):
+            yield list(range(0, int(i / 5 * df.shape[0]))) + list(range(int((i + 1) / 5 * df.shape[0]), df.shape[0])), \
+                list(range(int(i / 5 * df.shape[0]), df.shape[0], int((i + 1) / 5 * df.shape[0])))
+       
