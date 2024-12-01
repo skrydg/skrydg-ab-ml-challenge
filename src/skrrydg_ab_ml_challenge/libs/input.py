4e33176df2d2d08400c300bfa4704664c401fd9c
Aleksandr Mertsalov <skrrydg@ya.ru>
2024-12-01 13:40:04 +0400

    fix

--- src/skrrydg_ab_ml_challenge/libs/input.py
+++ src/skrrydg_ab_ml_challenge/libs/input.py
@@ -7,14 +7,16 @@ class Input:
     def __init__(self, env: Env):
         self.env = env
         
-    def get_train_dataset(self, n_rows=None, filters = []):
+    def get_train_dataset(self, n_rows=None, filters = [], columns=None):
         df = pl.scan_parquet(f"{self.env.input_directory}/ab-ml-challenge/train_data", n_rows=n_rows)
         for f in filters:
             df = df.filter(f)
+        df = df.select(columns)
         return Dataset({"md": df.collect()})
 
-    def get_test_dataset(self, n_rows=None, filters = []):
+    def get_test_dataset(self, n_rows=None, filters = [], columns=None):
         df = pl.scan_parquet(f"{self.env.input_directory}/ab-ml-challenge/test_data", n_rows=n_rows)
         for f in filters:
             df = df.filter(f)
+        df = df.select(columns)
         return Dataset({"md": df.collect()})
